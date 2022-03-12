b57959aed32ff178ab367273354fa902b3869ad9
Dwayne Whyte <18dwhyte.stem@gmail.com>
2022-03-12 15:33:58 -0500

    Navabr layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
 const Container = styled.div`
     height: 60px;
     
@@ -7,22 +9,68 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items:center;
     justify-content: spaced-between
 `;
 
+
+
+const Langauge = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const SearchContainer = styled.div `
+    border: 1px solid lightgray;
+    display:flex;
+    align-items: center;
+    margin:25px;
+    padding: 5px;
+    
+`
 const Left = styled.div`
-    flex: 1`
+    flex: 1;
+    display:flex;
+    align-items:center;
+    `
+
+const Input = styled.input`
+    border:none;
+`
+
 const Center = styled.div`
-    flex: 1`
+    flex: 1;
+    text-align:center;
+    `
+const Logo = styled.h1`
+    font-weight: bold;
+`
 const Right = styled.div`
-    flex: 1`
+    flex: 1;
+    `
+const MenuItem = styled.div`
+font-size:14px;
+cursor:pointer;
+`
 export default function Navbar() {
     return (
         <Container>
             <Wrapper>
-                  <Left>a</Left>
-                <Center>b</Center>
-                 <Right>c</Right>
+                <Left>
+                <Langauge>EN</Langauge>
+                <SearchContainer> 
+                    <Input></Input>
+                </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>Flowers.</Logo>
+                </Center>
+                 <Right>
+                     <MenuItem> REGISTER</MenuItem>
+                     <MenuItem> SIGN IN</MenuItem>
+                     <Badge badgeContent={4} color="success">
+                        <MailIcon color="action" />
+                     </Badge>
+                 </Right>
             </Wrapper>
         </Container>
     )
